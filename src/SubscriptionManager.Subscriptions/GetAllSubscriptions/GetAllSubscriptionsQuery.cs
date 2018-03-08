c3d4dd23276979d7f0607a98fce9d6c19627443d
Son Nguyen <dinh.son.nguyen@gmail.com>
2018-03-09 03:38:36 +0700

    Implement GetAllSubscriptions use case.

---
+++ src/SubscriptionManager.Subscriptions/GetAllSubscriptions/GetAllSubscriptionsQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using R2;
+
+namespace SubscriptionManager.Subscriptions.GetAllSubscriptions
+{
+    public class GetAllSubscriptionsQuery : IQuery<IList<SubscriptionDto>>
+    {
+    }
+
+    public class SubscriptionDto
+    {
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ src/SubscriptionManager.Subscriptions/GetAllSubscriptions/GetAllSubscriptionsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using R2;
+using Raven.Client;
+using Raven.Client.Linq;
+
+namespace SubscriptionManager.Subscriptions.GetAllSubscriptions
+{
+    public class GetAllSubscriptionsQueryHandler : QueryHandler<GetAllSubscriptionsQuery, IList<SubscriptionDto>>
+    {
+        private readonly IAsyncDocumentSession _session;
+
+        public GetAllSubscriptionsQueryHandler(IAsyncDocumentSession session)
+        {
+            _session = session;
+        }
+
+        protected override async Task<IList<SubscriptionDto>> HandleQueryAsync(GetAllSubscriptionsQuery query)
+        {
+            return
+                await _session.Query<Subscription>()
+                    .Select(x => new SubscriptionDto())
+                    .ToListAsync();
+        }
+    }
+}
